1d612556bcc41fc3ca5d873d53c41c30ad9b88a0
fanatsio <68788658+Fanatsio@users.noreply.github.com>
2023-01-24 14:49:29 +0500

    Add files via upload

---
+++ Python_Labs/06_songs_list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Есть список песен группы Depeche Mode со временем звучания с точностью до долей минут
+# Точность указывается в функции round(a, b)
+# где a, это число которое надо округлить, а b количество знаков после запятой
+# более подробно про функцию round смотрите в документации https://docs.python.org/3/search.html?q=round
+
+violator_songs_list = [
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83],
+]
+
+# распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
+#   Три песни звучат ХХХ.XX минут
+# Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
+# Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
+
+summa = round(violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1], 2)
+print("Три песни звучат", summa)
+
+# Есть словарь песен группы Depeche Mode
+violator_songs_dict = {
+    'World in My Eyes': 4.76,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.30,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.6,
+    'Policy of Truth': 4.88,
+    'Blue Dress': 4.18,
+    'Clean': 5.68,
+}
+
+# распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
+#   А другие три песни звучат ХХХ минут
+
+summa = round(violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress'], 2)
+print("Три песни звучат", summa)
