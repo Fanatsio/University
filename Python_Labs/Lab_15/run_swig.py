21d6eb7061da7f2702e1884f88bb0dc511fb8296
fanatsio <68788658+Fanatsio@users.noreply.github.com>
2023-01-24 15:15:01 +0500

    Add files via upload

---
+++ Python_Labs/Lab_15/run_swig.py
@@ -0,0 +1,6 @@
+from my import calc
+
+if __name__ == '__main__':
+    n = int(input('Enter n: '))
+    m = int(input('Enter m: '))
+    print(round(calc(n, m), 6))
---
+++ Python_Labs/Lab_15/steps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+time python3 task_1.py
+time ./task
+time python3 task_3.py
+time python3 run_swig.py
