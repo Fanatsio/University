1b831098ea3991fc4c3247b23bab2804851e8ef0
Fanatsio <68788658+Fanatsio@users.noreply.github.com>
2023-02-26 13:40:53 +0500

    Create lab1.sh

---
+++ SPO/lab1.sh
@@ -0,0 +1,18 @@
+echo "--------------------------Переменные оболочки--------------------------"
+for n in $@;
+	do
+    stat -c '%n' $n
+		stat -c '%x' $n
+		stat -c '%y' $n
+		stat -c '%w' $n
+  done
+echo "--------------------------gawk--------------------------"
+echo $(stat -c '%y' $n) $(stat -c '%w' $n) $(stat -c '%x' $n) | gawk '{print "File last modified time:",$1,$2,"\n","File creation time:",$4,$5,"\n","File last access time:",$7,$6}'
+echo "--------------------------Командная подстановка--------------------------"
+for n in $@;
+  do
+    script_name=`stat -c '%n' $n`
+    script_time=`stat -c '%x' $n; stat -c '%y' $n; stat -c '%w' $n;`
+    echo "$script_name 
+$script_time"
+  done
