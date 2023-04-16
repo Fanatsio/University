4706396cda2abb82207aead7149c961c11462068
Fanatsio <68788658+Fanatsio@users.noreply.github.com>
2023-04-16 18:56:50 +0500

    Update lab_4.c

--- C_Labs/lab_4.c
+++ C_Labs/lab_4.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
-double f(int n)
-{
-    double b, h, x, y;
+double f(int n){
+    double y;
     double I = 0.0;
-    for (double x = 1.0 / n; x <= 2*M_PI; x += 2.0 / n)
+    for (double x = 1.0 / n; x <= 2*3.1415; x += 2.0 / n)
     {
-        if (x <= M_PI / 2)
+        if (x <= 3.1415 / 2)
             y = pow(2, x) - 2 + x * x;
         else
-            y = sqrt(b + x) * exp(-((b + x) * (b + x)));
+            y = sqrt(x) * exp(-((x) * (x)));
         I += y;
     }
     I *= 2.0 / n;
     return I;
 }
 
-int main() 
-{
+int main() {
     double e;
-    printf("Введите точность >> ");
+    printf("Enter precision >> ");
     scanf("%lf", &e);
     int n = 1;
     double In = f(n), I2n = f(2*n);
     while((fabs(I2n - In) / 3) >= e)
     {
-        printf("%lf для n = %d \n", I2n, n*2);
+        printf("%lf for n = %d \n", I2n, n*2);
         n *= 2;
         In = I2n;
         I2n = f(2*n);
