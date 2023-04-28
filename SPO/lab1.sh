f31a03ed4bb383e063e4dd58ae47f70192ba59c6
Fanatsio <68788658+Fanatsio@users.noreply.github.com>
2023-04-28 19:19:04 +0500

    Update lab1.sh

--- SPO/lab1.sh
+++ SPO/lab1.sh
@@ -1,22 +1,35 @@
-echo "--------------------------Переменные оболочки--------------------------"
-for n in $@;
-	do
-    stat -c '%n' $n
-		stat -c '%x' $n
-		stat -c '%y' $n
-		stat -c '%w' $n
-  done
+if [ $# -eq 0 ]; then
+  echo "Не указаны имена файлов"
+  exit 1
+fi
+
+for filename in "$@"; do
+  if [ ! -f "$filename" ]; then
+    echo "Файл $filename не найден"
+    continue
+  fi
+
+  echo "Время создания файла $filename: $(stat -c %w "$filename")"
+  echo "Время последнего изменения файла $filename: $(stat -c %y "$filename")"
+  echo "Время последнего доступа к файлу $filename: $(stat -c %x "$filename")"
+  echo "----------------------------------------------------"
+done
+
+#2 ВЕРСИЯ
 echo "--------------------------gawk--------------------------"
-for n in $@;
-	do
-    stat -c '%n' $n
-    echo $(stat -c '%y' $n) $(stat -c '%w' $n) $(stat -c '%x' $n) | gawk '{print "File last modified time:",$1,$2,$3,"\n","File creation time:",$4,$5,$6,"\n","File last access time:",$7,$8,$9}'
-  done
-echo "--------------------------Командная подстановка--------------------------"
-for n in $@;
-  do
-    script_name=`stat -c '%n' $n`
-    script_time=`stat -c '%x' $n; stat -c '%y' $n; stat -c '%w' $n;`
-    echo "$script_name 
-$script_time"
-  done
+
+if [ $# -eq 0 ]; then
+  echo "Не указаны имена файлов"
+  exit 1
+fi
+
+for filename in "$@"; do
+  if [ ! -f "$filename" ]; then
+    echo "Файл $filename не найден"
+    continue
+  fi
+
+  stat -c '%n' $filename
+  echo $(stat -c '%y' $filename) $(stat -c '%w' $filename) $(stat -c '%x' $filename) | gawk '{print "Время создания файла:",$4,$5,$6,"\n","Время последнего изменения файла:",$1,$2,$3,"\n","Время последнего доступа к файлу:",$7,$8,$9}'
+  echo "----------------------------------------------------"
+done
