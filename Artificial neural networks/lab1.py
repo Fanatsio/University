c423fc1e1b0fce443fe184a9728cb729fcfcd771
Fanatsio <68788658+Fanatsio@users.noreply.github.com>
2023-09-07 21:17:23 +0500

    Update lab1.py

--- Artificial neural networks/lab1.py
+++ Artificial neural networks/lab1.py
@@ -7,20 +7,19 @@ class Perceptron:
     def train(self):
         for i in range(len(self.trainingSet)):
             d = self.trainingSet[i][2] - self.predict([self.trainingSet[i][0], self.trainingSet[i][1]])
-            if d >= 1:
+            if d <= 1:
                 self.weights[0] = self.weights[0] + d * self.trainingSet[i][0] * self.accuracy
                 self.weights[1] = self.weights[1] + d * self.trainingSet[i][1] * self.accuracy
 
     def test(self):
-        if (self.predict([0, 1]) == 1 and
+        if (self.predict([0, 0]) == 0 and
+                self.predict([0, 1]) == 0 and
                 self.predict([1, 0]) == 1 and
-                self.predict([1, 1]) == 1 and
-                self.predict([0, 0]) == 0):
+                self.predict([1, 1]) == 0):
             return 1
 
     def predict(self, x):
-        summator = x[0] * self.weights[0] + x[1] * self.weights[1]
-        if summator >= 1:
+        if x[0] * self.weights[0] > x[1] * self.weights[1]:
             return 1
         else:
             return 0
@@ -31,10 +30,10 @@ class Perceptron:
 
 def checking_values():
     print("Test values:")
-    print(f"1 or 0 = {neuron.predict([1, 0])}")
-    print(f"1 or 1 = {neuron.predict([1, 1])}")
-    print(f"0 or 1 = {neuron.predict([0, 1])}")
-    print(f"0 or 0 = {neuron.predict([0, 0])}")
+    print(f"1 >= 0 = {neuron.predict([1, 0])}")
+    print(f"1 >= 1 = {neuron.predict([1, 1])}")
+    print(f"0 >= 1 = {neuron.predict([0, 1])}")
+    print(f"0 >= 0 = {neuron.predict([0, 0])}")
 
 
 neuron = Perceptron()
