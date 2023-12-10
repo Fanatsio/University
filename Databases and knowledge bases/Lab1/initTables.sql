531653d96fde538fb8be1915848a3d5a08012dcb
Dmitriy Rechuk <rechuk04@mail.ru>
2023-12-10 12:16:15 +0500

    Update initTables.sql

--- Databases and knowledge bases/Lab1/initTables.sql
+++ Databases and knowledge bases/Lab1/initTables.sql
@@ -1,56 +1,48 @@
--- Database: warehouse
-
--- DROP DATABASE IF EXISTS warehouse;
-
-CREATE DATABASE warehouse
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Russian_Russia.1251'
-    LC_CTYPE = 'Russian_Russia.1251'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-	
 CREATE TABLE natural_person (
-	natural_person_id SERIAL PRIMARY KEY,
-	passport_details INTEGER UNIQUE NOT NULL,
-	phone_number INTEGER NOT NULL,
-	natural_customer_name VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
-	delivery_address VARCHAR NOT NULL
-)
+    natural_person_id SERIAL PRIMARY KEY,
+    passport_details INTEGER UNIQUE NOT NULL,
+    phone_number INTEGER NOT NULL,
+    natural_customer_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    delivery_address VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE legal_person (
-	legal_person_id SERIAL PRIMARY KEY,
-	inn INTEGER UNIQUE NOT NULL,
-	legal_customer_name VARCHAR NOT NULL,
-	phone_number INTEGER NOT NULL,
-	email VARCHAR NOT NULL,
-	delivery_address VARCHAR NOT NULL
-)
+    legal_person_id SERIAL PRIMARY KEY,
+    inn INTEGER UNIQUE NOT NULL,
+    legal_customer_name VARCHAR(255) NOT NULL,
+    phone_number INTEGER NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    delivery_address VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE provider (
-	provider_id SERIAL PRIMARY KEY,
-	inn INTEGER UNIQUE NOT NULL,
-	provider_name VARCHAR NOT NULL
-	address VARCHAR NOT NULL
-)
+    provider_id SERIAL PRIMARY KEY,
+    inn INTEGER UNIQUE NOT NULL,
+    provider_name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE product (
-	product_id SERIAL PRIMARY KEY,
-	place VARCHAR NOT NULL,
-	provider_name VARCHAR REFERENCES provider(provider_name),
-	product_name VARCHAR,
-	receipt_date DATE
-	quantity INT NOT NULL
-	price DECIMAL(10, 2) NOT NULL
-	weight DECIMAL(5, 2) NOT NULL
-)
-	
+    product_id SERIAL PRIMARY KEY,
+    place INTEGER UNIQUE NOT NULL,
+    provider_name VARCHAR(255) REFERENCES provider(provider_name),
+    product_name VARCHAR(255) NOT NULL,
+    receipt_date DATE,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    weight DECIMAL(5, 2) NOT NULL
+);
+
 CREATE TABLE orders (
-	orders_id SERIAL PRIMARY KEY,
-	order_number INTEGER UNIQUE NOT NULL,
-	name_customer VARCHAR REFERENCES 
-)
+    orders_id SERIAL PRIMARY KEY,
+    order_number INTEGER UNIQUE NOT NULL,
+    category_customer VARCHAR(50) NOT NULL CHECK (category_customer IN ('Юр. лицо', 'Физ. лицо')),
+    name_customer VARCHAR(255),
+    CONSTRAINT fk_legal_person FOREIGN KEY (name_customer) REFERENCES legal_person(legal_customer_name) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT fk_natural_person FOREIGN KEY (name_customer) REFERENCES natural_person(natural_customer_name) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT check_name_customer CHECK (
+        (category_customer = 'Юр. лицо' AND name_customer IS NOT NULL) OR
+        (category_customer = 'Физ. лицо' AND name_customer IS NOT NULL)
+    )
+);
