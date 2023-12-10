83887cb0b90fab64bcbd2f37a8a9d4639b80311b
Dmitriy Rechuk <rechuk04@mail.ru>
2023-12-10 14:18:27 +0500

    Update initTables.sql

--- Databases and knowledge bases/Lab1/initTables.sql
+++ Databases and knowledge bases/Lab1/initTables.sql
@@ -26,23 +26,68 @@ CREATE TABLE provider (
 CREATE TABLE product (
     product_id SERIAL PRIMARY KEY,
     place INTEGER UNIQUE NOT NULL,
-    provider_name VARCHAR(255) REFERENCES provider(provider_name),
+    provider_id INTEGER REFERENCES provider(provider_id),
     product_name VARCHAR(255) NOT NULL,
     receipt_date DATE,
     quantity INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
-    weight DECIMAL(5, 2) NOT NULL
+    weight DECIMAL(5, 2) NOT NULL,
+    provider_name VARCHAR(255),
+    CONSTRAINT fk_provider FOREIGN KEY (provider_id) REFERENCES provider(provider_id)
 );
+
+-- Заполним provider_name при помощи триггера
+CREATE OR REPLACE FUNCTION update_provider_name()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.provider_name := (
+        SELECT provider_name FROM provider WHERE provider.provider_id = NEW.provider_id
+    );
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_provider_name_trigger
+BEFORE INSERT OR UPDATE ON product
+FOR EACH ROW
+EXECUTE FUNCTION update_provider_name();
 
 CREATE TABLE orders (
     orders_id SERIAL PRIMARY KEY,
     order_number INTEGER UNIQUE NOT NULL,
+    product_id INTEGER,
+    product_name VARCHAR(255),
+    product_quantity INT NOT NULL,
+    weight DECIMAL(5, 2) NOT NULL,
+    registration_date DATE,
+    total_cost DECIMAL(10, 2) NOT NULL,
     category_customer VARCHAR(50) NOT NULL CHECK (category_customer IN ('Юр. лицо', 'Физ. лицо')),
-    name_customer VARCHAR(255),
-    CONSTRAINT fk_legal_person FOREIGN KEY (name_customer) REFERENCES legal_person(legal_customer_name) DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT fk_natural_person FOREIGN KEY (name_customer) REFERENCES natural_person(natural_customer_name) DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT check_name_customer CHECK (
-        (category_customer = 'Юр. лицо' AND name_customer IS NOT NULL) OR
-        (category_customer = 'Физ. лицо' AND name_customer IS NOT NULL)
+    customer_id INTEGER,
+    customer_name VARCHAR(255),
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(product_id),
+    CONSTRAINT fk_legal_person FOREIGN KEY (customer_id) REFERENCES legal_person(legal_person_id) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT fk_natural_person FOREIGN KEY (customer_id) REFERENCES natural_person(natural_person_id) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT check_customer_id CHECK (
+        (category_customer = 'Юр. лицо' AND customer_id IS NOT NULL) OR
+        (category_customer = 'Физ. лицо' AND customer_id IS NOT NULL)
+    ),
+    CONSTRAINT check_customer_name CHECK (
+        (category_customer = 'Юр. лицо' AND customer_name IS NOT NULL) OR
+        (category_customer = 'Физ. лицо' AND customer_name IS NOT NULL)
     )
 );
+
+CREATE OR REPLACE FUNCTION update_product_name()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.product_name := (
+        SELECT product_name FROM product WHERE product.product_id = NEW.product_id
+    );
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_product_name_trigger
+BEFORE INSERT OR UPDATE ON orders
+FOR EACH ROW
+EXECUTE FUNCTION update_product_name();
