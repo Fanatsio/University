01aab8ac5f243a7a5467b709677178e9185711b1
Dmitriy Rechuk <rechuk04@mail.ru>
2023-12-17 19:21:47 +0500

    update .sql

--- Databases and knowledge bases/Lab1/fillTestData.sql
+++ Databases and knowledge bases/Lab1/fillTestData.sql
@@ -1,17 +1,33 @@
+-- Создание или замена функции fill_data
 CREATE OR REPLACE FUNCTION fill_data() RETURNS void AS $$
 DECLARE
     i INTEGER := 0;
 BEGIN
     WHILE i < 1000 LOOP
-        INSERT INTO legal_person (inn, legal_customer_name, phone_number, email, delivery_address, representative_name) 
-		VALUES ('legal_customer' || i, 'phone_number' || i, 'email' || i, 'delivery_address' || i, 'representative_name' || i);
-		INSERT INTO natural_person (passport_details, phone_number, natural_customer_name, email, delivery_address) 
-		VALUES ('passport_details' || i, 'phone_number' || i, 'natural_customer_name' || i, 'email' || i, 'delivery_address' || i);
-		INSERT INTO product (place, product_name, receipt_date, article, quantity, price, weight) 
-		VALUES ('passport_details' || i, 'phone_number' || i, 'natural_customer_name' || i, 'email' || i, 'delivery_address' || i);
+        -- Исправленные операторы INSERT для таблицы legal_person
+        INSERT INTO legal_person (inn, name, phone, email, delivery_address, representative_forename) 
+        VALUES ('inn' || i, 'legal_customer_name' || i, 'phone_number' || i, 'email' || i, 'delivery_address' || i, 'representative_name' || i);
+
+        -- Исправленные операторы INSERT для таблицы natural_person
+        INSERT INTO natural_person (passport, forename, phone, email, delivery_address) 
+        VALUES ('passport_details' || i, 'natural_customer_name' || i, 'phone_number' || i, 'email' || i, 'delivery_address' || i);
+
+        -- Исправленные операторы INSERT для таблицы provider
+        INSERT INTO provider (inn, name, address) 
+        VALUES ('provider_inn' || i, 'provider_name' || i, 'provider_address' || i);
+
+        -- Исправленные операторы INSERT для таблицы product
+        INSERT INTO product (place, name, receipt_date, article, quantity, price, weight) 
+        VALUES (i, 'product_name' || i, CURRENT_DATE, 'article' || i, i, i * 10.5, i * 0.1);
+
+        -- Исправленные операторы INSERT для таблицы waybill
+        INSERT INTO waybill (article_product, product_quantity) 
+        VALUES ('article' || i, i);
+
         i := i + 1;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
+-- Вызов функции fill_data
 SELECT fill_data();
--- Databases and knowledge bases/Lab1/initTables.sql
+++ Databases and knowledge bases/Lab1/initTables.sql
@@ -1,77 +1,85 @@
+-- Создание таблицы natural_person
 CREATE TABLE natural_person (
     id SERIAL PRIMARY KEY,
-    passport VARCHAR(255) UNIQUE NOT NULL,
-    forename VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    delivery_address VARCHAR(255) NOT NULL
+    passport VARCHAR(255) UNIQUE,
+    forename VARCHAR(255),
+    phone VARCHAR(255),
+    email VARCHAR(255),
+    delivery_address VARCHAR(255)
 );
 
+-- Создание таблицы legal_person
 CREATE TABLE legal_person (
     id SERIAL PRIMARY KEY,
-    inn VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    delivery_address VARCHAR(255) NOT NULL,
-	representative_forename VARCHAR(255) NOT NULL
+    inn VARCHAR(255) UNIQUE,
+    name VARCHAR(255),
+    phone VARCHAR(255),
+    email VARCHAR(255),
+    delivery_address VARCHAR(255),
+	representative_forename VARCHAR(255)
 );
 
+-- Создание таблицы provider
 CREATE TABLE provider (
     id SERIAL PRIMARY KEY,
-    inn VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL
+    inn VARCHAR(255) UNIQUE,
+    name VARCHAR(255),
+    address VARCHAR(255)
 );
 
+-- Создание таблицы product
 CREATE TABLE product (
     id SERIAL PRIMARY KEY,
-    place INTEGER UNIQUE NOT NULL,
-    provider_id INTEGER REFERENCES provider(provider_id),
+    place INTEGER UNIQUE,
+    provider_id INTEGER REFERENCES provider(id),
     provider_name VARCHAR(255),
-    CONSTRAINT fk_provider FOREIGN KEY (provider_id) REFERENCES provider(provider_id),
-    name VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_provider FOREIGN KEY (provider_id) REFERENCES provider(id),
+    name VARCHAR(255),
     receipt_date DATE,
-    article VARCHAR(255) UNIQUE NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    weight DECIMAL(5, 2) NOT NULL,
+    article VARCHAR(255) UNIQUE,
+    quantity INT,
+    price DECIMAL(10, 2),
+    weight DECIMAL(5, 2)
 );
 
+-- Создание функции update_provider_name
 CREATE OR REPLACE FUNCTION update_provider_name()
 RETURNS TRIGGER AS $$
 BEGIN
     NEW.provider_name := (
-        SELECT provider_name FROM provider WHERE provider.provider_id = NEW.provider_id
+        SELECT name FROM provider WHERE provider.id = NEW.provider_id
     );
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+-- Создание триггера update_provider_name_trigger
 CREATE TRIGGER update_provider_name_trigger
 BEFORE INSERT OR UPDATE ON product
 FOR EACH ROW
 EXECUTE FUNCTION update_provider_name();
 
+-- Создание таблицы orders
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    order_number INTEGER UNIQUE NOT NULL,
-    weight DECIMAL(5, 2) NOT NULL,
+    order_number INTEGER UNIQUE,
+    weight DECIMAL(5, 2),
     registration_date DATE,
-    cost DECIMAL(10, 2) NOT NULL,
+    cost DECIMAL(10, 2),
     customer_id INTEGER,
-    customer_category VARCHAR(50) NOT NULL CHECK (category_customer IN ('Юр. лицо', 'Физ. лицо')),
-    CONSTRAINT fk_legal_person FOREIGN KEY (customer_id) REFERENCES legal_person(legal_person_id) DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT fk_natural_person FOREIGN KEY (customer_id) REFERENCES natural_person(natural_person_id) DEFERRABLE INITIALLY DEFERRED,
+    customer_category VARCHAR(50) CHECK (customer_category IN ('Юр. лицо', 'Физ. лицо')),
+    CONSTRAINT fk_legal_person FOREIGN KEY (customer_id) REFERENCES legal_person(id) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT fk_natural_person FOREIGN KEY (customer_id) REFERENCES natural_person(id) DEFERRABLE INITIALLY DEFERRED,
     CONSTRAINT check_customer_id CHECK (
-        (category_customer = 'Юр. лицо' AND customer_id IS NOT NULL) OR
-        (category_customer = 'Физ. лицо' AND customer_id IS NOT NULL)
+        (customer_category = 'Юр. лицо' AND customer_id IS NOT NULL) OR
+        (customer_category = 'Физ. лицо' AND customer_id IS NOT NULL)
     )
 );
 
+-- Создание таблицы waybill
 CREATE TABLE waybill (
     id SERIAL PRIMARY KEY,
     article_product VARCHAR(255) REFERENCES product(article),
-    product_quantity INTEGER NOT NULL,
+    product_quantity INTEGER,
 	orders_number INTEGER REFERENCES orders(order_number)
 );
