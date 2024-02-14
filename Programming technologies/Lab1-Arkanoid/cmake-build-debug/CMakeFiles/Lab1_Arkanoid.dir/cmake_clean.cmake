906d5e1daa79641d09cf80a0c803323701162f99
Dmitriy Rechuk <rechuk04@mail.ru>
2024-02-14 17:28:57 +0500

    global commit

---
+++ Programming technologies/Lab1-Arkanoid/cmake-build-debug/CMakeFiles/Lab1_Arkanoid.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1_Arkanoid.dir/Ball.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Ball.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/Brick.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Brick.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/Console.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Console.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/Game.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Game.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/GameObject.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/GameObject.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/Grid.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Grid.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/Level.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Level.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/NearlyEqual.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/NearlyEqual.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/Platform.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Platform.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/Time.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Time.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/Wall.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/Wall.cpp.obj.d"
+  "CMakeFiles/Lab1_Arkanoid.dir/main.cpp.obj"
+  "CMakeFiles/Lab1_Arkanoid.dir/main.cpp.obj.d"
+  "Lab1_Arkanoid.exe"
+  "Lab1_Arkanoid.exe.manifest"
+  "Lab1_Arkanoid.pdb"
+  "libLab1_Arkanoid.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab1_Arkanoid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
